a15cf5d26a2c87551524208da0a744c5d170b81f
Drurivera <71651374+Drurivera@users.noreply.github.com>
2022-03-16 23:44:36 -0600

    Add files via upload
    IMU ya envia datos

---
+++ IMU/Debug/MPU6050.d
@@ -0,0 +1,111 @@
+# FIXED
+
+MPU6050.obj: ../MPU6050.c
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdlib.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdio.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdarg.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/i2cm_drv.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/hw_mpu6050.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/mpu6050.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom_map.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h
+MPU6050.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/math.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_defs.h
+MPU6050.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_limits.h
+MPU6050.obj: ../MPU6050.h
+
+../MPU6050.c:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdlib.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdio.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdarg.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/i2cm_drv.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/hw_mpu6050.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/mpu6050.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom_map.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h:
+
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/math.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_defs.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_limits.h:
+
+../MPU6050.h:
+
---
+++ IMU/Debug/timer_spi_master_startup_ccs.d
@@ -0,0 +1,36 @@
+# FIXED
+
+timer_spi_master_startup_ccs.obj: C:/Users/Home/Downloads/C1_Lab7_archivos/timer_spi_master_startup_ccs.c
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+timer_spi_master_startup_ccs.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+
+C:/Users/Home/Downloads/C1_Lab7_archivos/timer_spi_master_startup_ccs.c:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
