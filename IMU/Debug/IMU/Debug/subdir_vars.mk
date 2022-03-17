8602b3911fe7df205171e0c6411a250bb15d49e7
Drurivera <71651374+Drurivera@users.noreply.github.com>
2022-03-17 02:07:34 -0600

    Add files via upload
    INICIO DEL PID

---
+++ IMU/Debug/IMU/Debug/subdir_vars.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+LIB_SRCS += \
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/ccs/Debug/driverlib.lib \
+C:/ti/TivaWare_C_Series-2.1.4.178/sensorlib/ccs/Debug/sensorlib.lib 
+
+C_SRCS += \
+../MPU6050.c \
+C:/Users/Home/Downloads/C1_Lab7_archivos/adc_timer_uart_startup_ccs.c \
+../main.c \
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.c 
+
+C_DEPS += \
+./MPU6050.d \
+./adc_timer_uart_startup_ccs.d \
+./main.d \
+./uartstdio.d 
+
+OBJS += \
+./MPU6050.obj \
+./adc_timer_uart_startup_ccs.obj \
+./main.obj \
+./uartstdio.obj 
+
+OBJS__QUOTED += \
+"MPU6050.obj" \
+"adc_timer_uart_startup_ccs.obj" \
+"main.obj" \
+"uartstdio.obj" 
+
+C_DEPS__QUOTED += \
+"MPU6050.d" \
+"adc_timer_uart_startup_ccs.d" \
+"main.d" \
+"uartstdio.d" 
+
+C_SRCS__QUOTED += \
+"../MPU6050.c" \
+"C:/Users/Home/Downloads/C1_Lab7_archivos/adc_timer_uart_startup_ccs.c" \
+"../main.c" \
+"C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.c" 
+
+
---
+++ IMU/Debug/IMU/main.c
@@ -0,0 +1,76 @@
+//*****************************************************************************
+//*****************************************************************************
+//   main.c
+//   Created on: 11/03/2022
+//   Author: Mariandree Rivera
+//   Carne: 18178
+//*****************************************************************************
+//*****************************************************************************
+
+//*****************************************************************************
+// Librerias
+//*****************************************************************************
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+#include "sensorlib/i2cm_drv.h"
+#include "sensorlib/hw_mpu6050.h"
+#include "sensorlib/mpu6050.h"
+
+#include "inc/hw_gpio.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_i2c.h"
+#include "inc/hw_sysctl.h"
+
+#include "driverlib/uart.h"
+#include "driverlib/debug.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/timer.h"
+#include "driverlib/adc.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/i2c.h"
+
+#include "utils/uartstdio.h"
+#include "MPU6050.h"
+#include <math.h>
+float x,y,z;
+
+//*****************************************************************************
+// Prototipo de funciones
+//*****************************************************************************
+void Timer0Handler(void)
+{
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+}
+void UART0IntHandler(void)
+{
+    UARTIntClear(UART0_BASE,UARTIntStatus(UART0_BASE, true)); //se limpia la bandera de la interrupcion
+
+}
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_PLL | SYSCTL_OSC_INT | SYSCTL_XTAL_16MHZ);
+
+        InitI2C0();
+        MPU6050INIT();
+
+        while(1)
+        {
+            MPU6050READDATA(&x,&y,&z);
+        }
+    return 0;
+}
